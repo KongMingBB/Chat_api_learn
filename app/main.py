2049a1e2390ad5302d7893b44d75dad509a5855e
Kong <1610427965@qq.com>
2026-06-17 23:33:01 +0800

    我新建了一个接口，feat： add health check endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
-
-
+class HealthResponse(BaseModel):
+    status: str
+    message: str
 @app.get("/")
 def root():
     return {"message": "FastAPI 项目启动成功"}
+@app.get('/heatch', response_model=HealthResponse)
+async def heatch_cheack()-> HealthResponse:
+    return HealthResponse(status="OK", message="Producet API is running")
