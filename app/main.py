25f5e877d6a3e38ddac2c53f2e06e1d40f887995
Kong <1610427965@qq.com>
2026-06-17 23:40:17 +0800

    我修改了拼写错误的

--- app/main.py
+++ app/main.py
@@ -7,6 +7,6 @@ class HealthResponse(BaseModel):
 @app.get("/")
 def root():
     return {"message": "FastAPI 项目启动成功"}
-@app.get('/heatch', response_model=HealthResponse)
-async def heatch_cheack()-> HealthResponse:
-    return HealthResponse(status="OK", message="Producet API is running")
+@app.get('/health', response_model=HealthResponse)
+async def health_check()-> HealthResponse:
+    return HealthResponse(status="OK", message="Product API is running")
