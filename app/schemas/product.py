0ea2bb7d59a6f47f32cbc1b11ec1cfcbca3534ff
Kong <1610427965@qq.com>
2026-06-18 01:13:39 +0800

    教研错误修改

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -5,22 +5,21 @@ from pydantic import BaseModel, ConfigDict, Field
 
 
 class ProductBase(BaseModel):
-    id: int = Field(gt=0)
     name:str = Field(min_length=1 ,max_length=100)
-    description:str | None
+    description:str | None=None
     price:Decimal = Field(gt=0)
     stock:int = Field(ge=0)
     is_active:bool = Field(default=True)
 class ProductCreate(ProductBase):
     pass
-class ProductUpdate(ProductBase):
-    name:str = Field(default=None,min_length=1 ,max_length=100)
+class ProductUpdate(BaseModel):
+    name:str |None= Field(min_length=1 ,max_length=100)
     description:str | None=None
     price:Decimal |Field(default=None,gt=0)
-    stock:int |Field(default=None,ge=0)
-    is_active:bool | Field(default=None)
+    stock:int |None=Field(default=None,ge=0)
+    is_active:bool | None=Field(default=None)
 class ProductResponse(ProductBase):
+    id:int =Field(gt=0)
     created_at:datetime
     updated_at:datetime
-
-model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True)
